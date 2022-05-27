58a76bf5c8e0ce8a3e65fb6fa72011ba61b3f532
Manpreet <preetr4ha@gmail.com>
2022-05-27 11:34:27 -0600

    working yet to understand

--- LetterCombs.java
+++ LetterCombs.java
@@ -1,44 +1,32 @@
 import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class LetterCombs {
-
-
-    public static HashMap<String, String> getDigitMapping() {
-        HashMap<String, String> digitMapping = new HashMap<String, String>();
-        digitMapping.put("1", "abc");
-        digitMapping.put("2", "def");
-        digitMapping.put("3", "ghi");
-        digitMapping.put("4", "jkl");
-        digitMapping.put("5", "mno");
-        digitMapping.put("6", "pqrs");
-        digitMapping.put("7", "tuv");
-        digitMapping.put("8", "wxyz");
-        return digitMapping;
-    }
-
     public static List<String> letterCombinations(String digits) {
-        HashMap<String,String> digitMapping = getDigitMapping();
-        List<String> output = new ArrayList<>();
-        List<String> temp = new ArrayList<>();
-        // put all the chars the digits can produce in temp
-        for(char c: digits.toCharArray()){
-            if(digitMapping.get(c) != null){
-                temp.add(digitMapping.get(c));
+        LinkedList<String> output_arr = new LinkedList<>();
+        if (digits.length() == 0) return output_arr;
+        output_arr.add("");
+        String[] char_map = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv",
+                "wxyz"
+        };
+
+        for(int i = 0; i<digits.length(); i++){
+            // index has each characters digit value
+            int index = Character.getNumericValue(digits.charAt(i));
+
+
+            while (output_arr.peek().length() == i){
+                String permutation = output_arr.remove();
+                for(char c: char_map[index].toCharArray()){
+                    output_arr.add(permutation + c);
+                }
             }
+
         }
 
-        for(int i = 0; i<temp.size(); i++){
-            for(int j = 0; j<temp.get(i).length(); j++){
 
-            }
-        }
 
-        String[] haha = {"2", "3"};
-        return output;
+        return output_arr;
     }
 
 }
--- Main.java
+++ Main.java
@@ -3,8 +3,6 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-//        System.out.println(LongestSubstringWithoutRepeatingChars.find("abcabcbb"
-//));
-        System.out.println(LetterCombs.letterCombinations("13"));
+        System.out.println(LetterCombs.letterCombinations("234"));
     }
 }
